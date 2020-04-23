e74f81f65777e9c6a7e8199726406e9dd4610271
xdream <xdream@gmail.com>
2020-04-23 17:48:26 +0800

    make things simple.

---
+++ loggingTest.py
@@ -0,0 +1,20 @@
+from xlogger import get_my_logger
+
+logger = get_my_logger(__name__)
+
+
+def main():
+    logger.info('这是一条中文消息')
+    logger.debug('Debugging')
+    logger.warning('Warning exists')
+    try:
+        result = 10 / 0
+    except ZeroDivisionError:
+        logger.error('Faild to get result', exc_info=True)
+    logger.info('Finish')
+    logger.debug('Hello %s, %s!' % ('World', 'Congratulations'))
+    logger.critical('Hello %s, %s!' % ('World', 'Congratulations'))
+
+
+if __name__ == '__main__':
+    main()
